1271533254a100520356d8861e4aa1d09c498e0d
9087000 <50361103+9087000@users.noreply.github.com>
2024-10-17 14:02:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+    let radius = document.getElementById('radius').value;
+    console.log(radius);
+    let ans;
+    if (radius) {
+        ans = (4 * Math.PI * Math.pow(radius, 3)) / 3;
+    }
+    let volume = document.getElementById('volume');
+    if (ans != undefined) {
+        volume.value = ans.toFixed(4);
+    }
+    return false;
+}
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+
+// -----------------
+// let clickfn = document.getElementById("submit");
+// clickfn.addEventListener("click", getVol);
+
+// function getVol(){
+//   let radius = document.getElementById("radius").value;
+//   let volume = 4/3*3.14*radius*radius*radius;
+//   console.log(volume);
+//   document.getElementById("volume").innerText = volume;
+// }
+// // getVol();
